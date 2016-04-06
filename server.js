d9ce49df520f9079e9bca3692d70d9a3cbd502ce
Chad Killingsworth <chadkillingsworth@missouristate.edu>
2016-04-06 14:15:04 -0500

    Add server rewriting to support single page application urls

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var connect = require('connect');
 var http = require('http');
 var serveStatic = require('serve-static');
+var fs = require('fs');
+
+var url = require('url');
 
 var app = connect();
+
 var serve = serveStatic('./');
 app.use(serve);
+
+// Map all the expected SPA urls to the root index.html
+app.use(function (req, res, next) {
+  var reqUri = url.parse(req.url);
+  if (/^\/[0-9]+/.test(reqUri.pathname)) {
+    fs.readFile('./index.html', { encoding: 'utf8' }, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Content-Length', data.length);
+      res.write(data, 'utf8', function (err) {
+        if (err) {
+          throw err;
+        }
+        res.end();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 console.log('Starting webserver on http://localhost:8080/');
 http.createServer(app).listen(8080);
